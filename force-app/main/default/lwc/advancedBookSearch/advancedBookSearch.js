bf12f03fa09a5b12bd85f354c2a230b75c2a1bc6
Milica Vacic <mvacic9@gmail.com>
2023-05-11 15:55:06 +0200

    LIB-7 in progress

--- force-app/main/default/lwc/advancedBookSearch/advancedBookSearch.js
+++ force-app/main/default/lwc/advancedBookSearch/advancedBookSearch.js
@@ -1,3 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getDigitalBooks from '@salesforce/apex/BookController.getDigitalBooks';
+import confirmDigitalBookPurchase from '@salesforce/apex/BookController.confirmDigitalBookPurchase';
+
 
 export default class AdvancedBookSearch extends LightningElement {}
