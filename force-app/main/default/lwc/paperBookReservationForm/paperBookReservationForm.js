f873c9e0948218652d4910c8c30041ddbca7bb83
SasaCejic <cejicsasa17@gmail.com>
2023-05-17 21:30:51 +0200

    LIB-219 Completed LWC for paper book reservation

--- force-app/main/default/lwc/paperBookReservationForm/paperBookReservationForm.js
+++ force-app/main/default/lwc/paperBookReservationForm/paperBookReservationForm.js
@@ -1,3 +1,55 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import LightningConfirm from 'lightning/confirm';
+import getBookById from '@salesforce/apex/BookController.getBookById';
 
-export default class PaperBookReservationForm extends LightningElement {}
+export default class PaperBookReservationForm extends LightningElement {
+    bookId = '';
+    selectedBook;
+
+    @wire(getBookById, {bookId: '$bookId'})
+    wiredBook({data, error}){
+        if(data){
+            this.selectedBook = data;
+        }
+    }
+
+    async onSubmitHandler (event) {
+        event.preventDefault();
+        const fields = event.detail.fields;
+        this.bookId = fields.Book__c;
+
+        const result = await LightningConfirm.open({
+            message:`Book: ${this.selectedBook.Name},    Total Price: ${this.selectedBook.Price__c * fields.Quantity__c},    Quantity: ${fields.Quantity__c}`,
+            variant: 'headerless',
+            label: 'Book Reservation',
+        });
+
+        if (result) {
+            this.template.querySelector('lightning-record-form').submit(fields);
+        }
+    }
+
+    closeForm (event) {
+        window.history.back();
+    }
+
+    showSuccessMessage (event) {
+        this.toast('Success', 'Book reservation has been made successfully!');
+    }
+
+    showErrorMessage (event) {
+        this.toast('Error', 'Record cannot be created', 'error', 'dismissable');
+    }
+
+    toast (title, message, variant, mode) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: title,
+                message: message,
+                variant: variant,
+                mode: mode
+            })
+        );
+    }
+}
