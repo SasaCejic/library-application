e502b060186eda066e5f769e54a4f784d2e2087a
Milica Vacic <mvacic9@gmail.com>
2023-05-18 11:37:09 +0200

    LIB-7 book search without price

--- force-app/main/default/lwc/advancedBookSearch/advancedBookSearch.js
+++ force-app/main/default/lwc/advancedBookSearch/advancedBookSearch.js
@@ -1,11 +1,9 @@
 import { LightningElement, api, wire } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import NAME_FIELD from '@salesforce/schema/Book__c.Name';
 import ISBN_FIELD from '@salesforce/schema/Book__c.ISBN__c';
 import AUTHOR_FIELD from '@salesforce/schema/Book__c.Author__c';
 import PUBLISH_DATE_FIELD from '@salesforce/schema/Book__c.Publish_Date__c';
-//import BOOKSTORE_FIELD from '@salesforce/schema/Book__c.Bookstore__c';
 import CATEGORIES_FIELD from '@salesforce/schema/Book__c.Categories__c';
 import LANGUAGE_FIELD from '@salesforce/schema/Book__c.Language__c';
 import DESCRIPTION_FIELD from '@salesforce/schema/Book__c.Description__c';
@@ -13,6 +11,12 @@ import DESCRIPTION_FIELD from '@salesforce/schema/Book__c.Description__c';
 import AUTHOR_OBJECT from '@salesforce/schema/Author__c';
 import BOOKSTORE_OBJECT from '@salesforce/schema/Bookstore__c';
 import PUBLISHER_OBJECT from '@salesforce/schema/Publisher__c';
+import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
+import BOOK_OBJECT from '@salesforce/schema/Book__c';
+import CATEGORIES_PICKLIST from '@salesforce/schema/Book__c.Categories__c';
+import LANGUAGE_PICKLIST from '@salesforce/schema/Book__c.Language__c'
+import getBooksFromSearchDTO from '@salesforce/apex/BookController.getBooksFromSearchDTO';
+
 
 export default class AdvancedBookSearch extends LightningElement {
     //object api names for autocompleteComoboxWrappers
@@ -66,7 +70,7 @@ export default class AdvancedBookSearch extends LightningElement {
             type: 'text',
             sortable: true
         },
-/*        {
+/*      {
             label: 'Price',
             fieldName: PRICE_FIELD.fieldApiName,
             type: 'text',
@@ -77,12 +81,6 @@ export default class AdvancedBookSearch extends LightningElement {
     sortBy;
     // Direction to sort book record table in
     sortDirection;
-    // List that holds objects with labels and values for the author combobox
-    authorOptions = [];
-    // List that holds objects with labels and values for the bookstore combobox
-    bookstoreOptions = [];
-    // List that holds objects with labels and values for the publisher combobox
-    publisherOptions = [];
     // List that holds values for he languages listbox
     languagesOptions=[];
     // List that holds values for he categories listbox
@@ -119,7 +117,49 @@ export default class AdvancedBookSearch extends LightningElement {
     books=undefined;
     //Maximum of the price slider
     priceRangeMaximum=500;
+    //book default record id
+    bookDefaultRecordTypeId;
+    //number of book records to retrieve in a single method call
+    tableLoadStep=10;
+    //offset when retrieving book records
+    tableOffset=0;
+    //value indicating if more book records can still be retrieved
+    moreToLoad=true;
+    //DTO used for passing parameters to controller
+    bookSearchDTO={};
+    //boolean determening if book search returned empty result
+    resultsNotFound=false;
 
+
+    /*
+     * Gets book object info
+     */
+    @wire(getObjectInfo, { objectApiName: BOOK_OBJECT })
+    bookObjectInfo;
+
+    /*
+     * Gets values for categories picklist options
+     */
+    @wire(getPicklistValues, { recordTypeId: '$bookObjectInfo.data.defaultRecordTypeId', fieldApiName: CATEGORIES_PICKLIST })
+    wireCategoriesPicklist({data, error}) {
+        if (data) {
+            this.categoriesOptions = [...data.values];
+        } else if (error) {
+            this.showToast('Error', 'Error while retrieving categories', 'error');
+        }
+    }
+
+    /*
+     * Gets values for languages picklist options
+     */
+    @wire(getPicklistValues, { recordTypeId: '$bookObjectInfo.data.defaultRecordTypeId', fieldApiName: LANGUAGE_PICKLIST })
+    wireLanguagesPicklist({data, error}) {
+        if (data) {
+            this.languagesOptions = [...data.values];
+        } else if (error) {
+            this.showToast('Error', 'Error while retrieving languages', 'error');
+        }
+    }
 
     /*
      * Methods setting the values of page inputs, comboboxes and listviews to corresponding variables
@@ -162,12 +202,103 @@ export default class AdvancedBookSearch extends LightningElement {
 
     }
 
-    handleLoadMore(){
+    handleLoadMore(event){
+        this.tableOffset=this.books.length;
 
+        if (this.moreToLoad){
+            event.target.isLoading=true;
+            this.makeSearchRequest()
+        }
     }
 
+    /*
+     * Sets data parameters for search
+     * Calls makeSearchRequest() function
+     */
     handleBookSearch(){
+        if (this.template.querySelector('lightning-datatable')) {
+                this.template.querySelector('lightning-datatable').isLoading=true
+        };
+        //set DTO field values and reset data
+        this.bookSearchDTO={
+            name:this.name,
+            ISBN:this.ISBN,
+            author:this.author,
+            publishDate:this.publishDate,
+            bookstore:this.bookstore,
+            publisher:this.publisher,
+            categories:this.categories,
+            languages:this.languages,
+            price:this.price,
+            reviewScore:this.reviewScore,
+            term:this.term
+        }
+        this.tableOffset=0;
+        this.books=[];
+
+        // Apex call
+        this.makeSearchRequest();
+    }
+
+    /*
+     * Queries books from database matching all the parameters that were submited
+     * Parameters are sent to controller in the form of a a BookSearchDTO object
+     * Implements lazy loading
+     */
+    makeSearchRequest(){
+        getBooksFromSearchDTO({ bookSearchDTO: this.bookSearchDTO, limitSize:this.tableLoadStep, offset:this.tableOffset }).then((data) => {
+
+            let mappedBooks = data.map(recievedBook=>{
+                let mappedBook={};
+
+                for(const field in recievedBook){
+                    mappedBook[field]=recievedBook[field];
+                }
+                mappedBook.NameUrl=`/`+recievedBook.Id;
+                return mappedBook;
+            });
+            this.books=[...this.books,...mappedBooks];
+            this.resultsNotFound=(this.books.length==0);
+            console.log(this.books);
+
+            //if (this.sortBy) this.sortData(this.sortFieldName, this.sortDirection, this.nullToEmpty);
+            if (mappedBooks.length<this.tableLoadStep) {
+                this.moreToLoad=false;
+            }
+            else {
+                this.moreToLoad=true;
+            }
+        }).catch((err) => {
+            // Handle different type of errors
+            if (err.body.message) {
+                this.showToast('Error', err.body.message, 'error');
+            } else if (err.body.pageErrors) {
+                this.showToast('Error', err.body.pageErrors[0].message, 'error');
+            } else {
+                this.showToast('Error', 'Error while searching books', 'error');
+            }
+        }).finally(() => {
+
+            if (this.template.querySelector('lightning-datatable')) {
+                this.template.querySelector('lightning-datatable').isLoading=false
+            };
+        })
 
     }
+
+    /**
+     * @param title - title of toast message
+     * @param message - message of toast message
+     * @param varian - varian of toast message
+     */
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant
+        });
+        this.dispatchEvent(event);
+    }
+
 
 }
